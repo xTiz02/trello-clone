339efb1abd5f0d06a08d265ff67e7ba311557f0b
Piero Alessandro Rodriguez Diaz <u21231696@utp.edu.pe>
2025-03-28 10:44:49 -0500

    commit 1

---
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  faBell,
+  faInfoCircle,
+  faClose,
+  faAngleDown
+} from '@fortawesome/free-solid-svg-icons';
+
+import { AuthService } from '@services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
+  faClose = faClose;
+  faAngleDown = faAngleDown;
+
+  isOpenOverlayAvatar = false;
+  isOpenOverlayBoards = false;
+
+  user$ = this.authService.user$;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+}
